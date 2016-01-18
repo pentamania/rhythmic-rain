9a53c20d5053fa0e8285da04ad46905712518beb
pentamania <tanigami7220@gmail.com>
2016-01-19 00:42:43 +0900

    modified game/index.html, etc.

--- game/loader.js
+++ game/loader.js
@@ -1,16 +1,40 @@
 //
+var getAudioBuffer = function(context, url, fn) {
+    var req = new XMLHttpRequest();
+    // array buffer を指定
+    req.responseType = 'arraybuffer';
+    console.log(context);
+    req.onreadystatechange = function() {
+        if (req.readyState === 4) {
+            if (req.status === 0 || req.status === 200) {
+                // array buffer を audio buffer に変換
+                context.decodeAudioData(req.response, function(buffer) {
+                    // コールバックを実行
+                    fn(buffer);
+                });
+            }
+        }
+    };
+
+    req.open('GET', url, true);
+    req.send(null);
+};
+
 var _soundPreload = function(srcObj, register, fn){
     var _keys = Object.keys(srcObj);
-    var context = false
+    // var context = window.AudioContext || window.webkitAudioContext;
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    var context = new AudioContext();
+
     _keys.forEach(function(key){
         if (context)
         {
             // webAudio
-            getAudioBuffer(srcObj[key], function(buffer){
+            getAudioBuffer(context, srcObj[key], function(buffer){
                 register[key] = buffer; // 登録
                 console.log("webAudio",Object.keys(register).length+" , 全音源:"+Object.keys(SOUND_ASSETS).length);
-                if (Object.keys(register).length === _keys.length) fn(); //コールバック実行
-            })
+                if (Object.keys(register).length === _keys.length) fn(); //ロード完了後コールバック実行
+            });
         } else
         {
             // html5audio
